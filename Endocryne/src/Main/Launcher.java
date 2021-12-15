ee56a8748a2700fcbc4156d263b8c56cc85a4085
Mika <62205209+mikxth@users.noreply.github.com>
2021-12-15 12:49:52 +0100

    Update Launcher.java

--- Endocryne/src/Main/Launcher.java
+++ Endocryne/src/Main/Launcher.java
@@ -2,6 +2,8 @@ package Main;
 
 public class Launcher {
 	
+	public static boolean DEBUG = true;
+	
 	public static void main(String[] args) {
 		/*TitleScreen screen = new TitleScreen();
 		screen.setVisible(true);*/
