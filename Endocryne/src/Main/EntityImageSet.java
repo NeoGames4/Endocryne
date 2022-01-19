a2b79cebd8c233a0be03af44d5b249859b938784
Mika <62205209+mikxth@users.noreply.github.com>
2022-01-19 13:22:41 +0100

    Update

--- Endocryne/src/Main/EntityImageSet.java
+++ Endocryne/src/Main/EntityImageSet.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 public class EntityImageSet {
 	
@@ -12,6 +13,9 @@ public class EntityImageSet {
 	public final Image jump;
 	public final Image hit;
 	
+	public static final int width = 40;
+	public static final int height = 120;
+	
 	public EntityImageSet(Image defaultImage, Image leftOne, Image rightOne, Image leftTwo, Image rightTwo, Image jump, Image hit) {
 		this.defaultImage = defaultImage;
 		this.leftOne = leftOne;
@@ -20,6 +24,25 @@ public class EntityImageSet {
 		this.rightTwo = rightTwo;
 		this.jump = jump;
 		this.hit = hit;
+	}
+	
+	/**
+	 * Konvertiert ein Bild in ein EntityImageSet.
+	 * @param image ein Bildset
+	 * @return das das resultierende {@link #getEntityImageSet(Image)}
+	 */
+	public static EntityImageSet getEntityImageSet(Image image) throws Exception {
+		if(image.getWidth(null) == 7 * width && image.getHeight(null) == height) {
+			BufferedImage img = (BufferedImage) image;
+			Image defaultImage = img.getSubimage(0, 0, width, height);
+			Image leftOne = img.getSubimage(width, 0, width, height);
+			Image rightOne = img.getSubimage(2 * width, 0, width, height);
+			Image leftTwo = img.getSubimage(3 * width, 0, width, height);
+			Image rightTwo = img.getSubimage(4 * width, 0, width, height);
+			Image jump = img.getSubimage(5 * width, 0, width, height);
+			Image hit = img.getSubimage(6 * width, 0, width, height);
+			return new EntityImageSet(defaultImage, leftOne, rightOne, leftTwo, rightTwo, jump, hit);
+		} else throw new Exception("Image has to be scaled like " + (7 * width) + " px times " + height + " px");
 	}
 
 }
